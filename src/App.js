89f910ce74a022dfc210e8f995590fc0ef226f4a
Anubhuti Shruti <shrutianubhuti@gmail.com>
2020-07-25 09:48:34 +0530

    Form Validation by ReactJs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+    import React, { Component } from 'react';    
+    import './App.css';    
+    import AdmissionForm from './AdmissionForm';    
+        
+    export default class App extends Component {    
+      render() {    
+        return (    
+          <div className="content">    
+            <AdmissionForm />    
+          </div >    
+        );    
+      }    
+    }
